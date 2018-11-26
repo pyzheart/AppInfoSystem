66e47a13d850a6c80e820b40ec95662e3abf0b8b
pyzheart <pyz2220222884@163.com>
2018-11-26 16:15:53 +0800

    AppInfoSys-添加了一个测试jsp文件

--- src/cn/appsys/pojo/AdPromotion.java
+++ src/cn/appsys/pojo/AdPromotion.java
@@ -1,7 +1,7 @@
 package cn.appsys.pojo;
 
 
-import java.sql.Timestamp;
+import java.util.Date;
 
 public class AdPromotion {
 
@@ -10,110 +10,14 @@ public class AdPromotion {
   private String adPicPath;
   private long adPv;
   private long carouselPosition;
-  private Timestamp startTime;
-  private Timestamp endTime;
+  private Date startTime;
+  private Date endTime;
   private long createdBy;
-  private Timestamp creationDate;
+  private Date creationDate;
   private long modifyBy;
-  private Timestamp modifyDate;
+  private Date modifyDate;
 
 
-  public long getId() {
-    return id;
-  }
 
-  public void setId(long id) {
-    this.id = id;
-  }
-
-
-  public long getAppId() {
-    return appId;
-  }
-
-  public void setAppId(long appId) {
-    this.appId = appId;
-  }
-
-
-  public String getAdPicPath() {
-    return adPicPath;
-  }
-
-  public void setAdPicPath(String adPicPath) {
-    this.adPicPath = adPicPath;
-  }
-
-
-  public long getAdPv() {
-    return adPv;
-  }
-
-  public void setAdPv(long adPv) {
-    this.adPv = adPv;
-  }
-
-
-  public long getCarouselPosition() {
-    return carouselPosition;
-  }
-
-  public void setCarouselPosition(long carouselPosition) {
-    this.carouselPosition = carouselPosition;
-  }
-
-
-  public java.sql.Timestamp getStartTime() {
-    return startTime;
-  }
-
-  public void setStartTime(java.sql.Timestamp startTime) {
-    this.startTime = startTime;
-  }
-
-
-  public java.sql.Timestamp getEndTime() {
-    return endTime;
-  }
-
-  public void setEndTime(java.sql.Timestamp endTime) {
-    this.endTime = endTime;
-  }
-
-
-  public long getCreatedBy() {
-    return createdBy;
-  }
-
-  public void setCreatedBy(long createdBy) {
-    this.createdBy = createdBy;
-  }
-
-
-  public java.sql.Timestamp getCreationDate() {
-    return creationDate;
-  }
-
-  public void setCreationDate(java.sql.Timestamp creationDate) {
-    this.creationDate = creationDate;
-  }
-
-
-  public long getModifyBy() {
-    return modifyBy;
-  }
-
-  public void setModifyBy(long modifyBy) {
-    this.modifyBy = modifyBy;
-  }
-
-
-  public java.sql.Timestamp getModifyDate() {
-    return modifyDate;
-  }
-
-  public void setModifyDate(java.sql.Timestamp modifyDate) {
-    this.modifyDate = modifyDate;
-  }
 
 }
